8cec4be7f3484bf141cd5eaed583dfcedeea76a3
Joshua Sommers <js3289@nau.edu>
2016-05-16 16:26:14 -0700

    Modified game.js to work through the Player class
    Created a player class that will encapsulate all of the information
    about the player.
    
    Currently not working - onclick methods of player class.

--- game.js
+++ game.js
@@ -1,37 +1,54 @@
 var gameport = document.getElementById("gameport");
-
 var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x3344ee});
 gameport.appendChild(renderer.view);
 
+
 var stage = new PIXI.Container();
 
-var texture = PIXI.Texture.fromImage("Assets/png/Character-sprite.png");
 
-var character = new PIXI.Sprite(texture);
-
-var isClicked = false;
-
-var movement = 3;
-
-character.anchor.x = 0.5;
-character.anchor.y = 0.5;
-
-character.position.x = 200;
-character.position.y = 200;
-
-character.interactive = true;
+class Player {
+	constructor(path) {
 	
-character
-	.on('mousedown', onButtonDown)
-	.on('mouseup', onButtonUp)
-	.on('mouseupoutside', onButtonUp)
-	.on('touchstart', onButtonDown)
-	.on('touchend', onButtonUp)
-	.on('touchendoutside', onButtonUp);
+	this.movement = 3;
+	this.isClicked = false;
 	
+	this.temp = PIXI.Texture.fromImage(path);
+	this.sprite = new PIXI.Sprite(this.temp);
+	this.sprite.interactive = true;
+	
+	this.sprite.anchor.x = 0.5;
+	this.sprite.anchor.y = 0.5;
+	this.sprite.position.x = 200;
+	this.sprite.position.y = 200;
+	
+	this.sprite
+		.on('mousedown', this.onButtonDown)
+		.on('mouseup', this.onButtonUp)
+		.on('mouseupoutside', this.onButtonUp)
+		.on('touchstart', this.onButtonDown)
+		.on('touchend', this.onButtonUp)
+		.on('touchendoutside', this.onButtonUp);
+	
+	}
+	
+	onButtonDown() {
+		if(!this.isClicked) {
+			this.sprite.y -= 100;
+			this.isClicked = true;
+		}
+	}
+
+	onButtonUp() {
+		if(this.isClicked) {
+			this.sprite.y += 100;
+			this.isClicked = false;
+		}
+	}
+}
 		
+var character = new Player("Assets/png/Character-sprite.png");
 
-stage.addChild(character);
+stage.addChild(character.sprite);
 
 document.addEventListener('keydown', onKeyDown);
 
@@ -40,47 +57,28 @@ function animate() {
 	renderer.render(stage);
 }
 
-function onButtonDown() {
-	if(!isClicked) {
-		character.y -= 100;
-		isClicked = true;
-	}
-}
 
-function onButtonUp() {
-	if(isClicked) {
-		character.y += 100;
-		isClicked = false;
-	}
-}
 
 function onKeyDown(key) {
-    // W Key is 87
-    // Up arrow is 38
+    // W Key is 87 Up arrow is 38
     if (key.keyCode === 87 || key.keyCode === 38) {
-			character.y -= movement;
+			character.sprite.y -= character.movement;
         }
   
-    // S Key is 83
-    // Down arrow is 40
+    // S Key is 83 Down arrow is 40
     else if (key.keyCode === 83 || key.keyCode === 40) {
-			character.y += movement;
+			character.sprite.y += character.movement;
         }
   
-    // A Key is 65
-    // Left arrow is 37
+    // A Key is 65 Left arrow is 37
     else if (key.keyCode === 65 || key.keyCode === 37) {
-			character.x -= movement;
+			character.sprite.x -= character.movement;
         }
   
-    // D Key is 68
-    // Right arrow is 39
+    // D Key is 68 Right arrow is 39
     else if (key.keyCode === 68 || key.keyCode === 39) {
-			character.x += movement;
+			character.sprite.x += character.movement;
     }
 }
-
-
-	
 	
 animate();
