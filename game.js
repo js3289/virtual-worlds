074fecf6db65cca23f180c8679cc3901a18ff4db
Joshua Sommers <js3289@nau.edu>
2016-05-16 17:18:10 -0700

    Fixed OOP implementation
    Changed functions that weren't working at last commit. Everything is
    working again.

--- game.js
+++ game.js
@@ -8,19 +8,19 @@ var stage = new PIXI.Container();
 
 class Player {
 	constructor(path) {
-	
+
 	this.movement = 3;
 	this.isClicked = false;
-	
+
 	this.temp = PIXI.Texture.fromImage(path);
 	this.sprite = new PIXI.Sprite(this.temp);
 	this.sprite.interactive = true;
-	
+
 	this.sprite.anchor.x = 0.5;
 	this.sprite.anchor.y = 0.5;
 	this.sprite.position.x = 200;
 	this.sprite.position.y = 200;
-	
+
 	this.sprite
 		.on('mousedown', this.onButtonDown)
 		.on('mouseup', this.onButtonUp)
@@ -28,24 +28,24 @@ class Player {
 		.on('touchstart', this.onButtonDown)
 		.on('touchend', this.onButtonUp)
 		.on('touchendoutside', this.onButtonUp);
-	
+
 	}
-	
+
 	onButtonDown() {
-		if(!this.isClicked) {
-			this.sprite.y -= 100;
-			this.isClicked = true;
+		if(!character.isClicked) {
+			character.sprite.y -= 100;
+			character.isClicked = true;
 		}
 	}
 
 	onButtonUp() {
-		if(this.isClicked) {
-			this.sprite.y += 100;
-			this.isClicked = false;
+		if(character.isClicked) {
+			character.sprite.y += 100;
+			character.isClicked = false;
 		}
 	}
 }
-		
+
 var character = new Player("Assets/png/Character-sprite.png");
 
 stage.addChild(character.sprite);
@@ -62,23 +62,23 @@ function animate() {
 function onKeyDown(key) {
     // W Key is 87 Up arrow is 38
     if (key.keyCode === 87 || key.keyCode === 38) {
-			character.sprite.y -= character.movement;
-        }
-  
+		character.sprite.y -= character.movement;
+    }
+
     // S Key is 83 Down arrow is 40
     else if (key.keyCode === 83 || key.keyCode === 40) {
-			character.sprite.y += character.movement;
-        }
-  
+		character.sprite.y += character.movement;
+    }
+
     // A Key is 65 Left arrow is 37
     else if (key.keyCode === 65 || key.keyCode === 37) {
-			character.sprite.x -= character.movement;
-        }
-  
+		character.sprite.x -= character.movement;
+    }
+
     // D Key is 68 Right arrow is 39
     else if (key.keyCode === 68 || key.keyCode === 39) {
-			character.sprite.x += character.movement;
+		character.sprite.x += character.movement;
     }
 }
-	
+
 animate();
