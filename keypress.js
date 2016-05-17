a8bca6eef750a5586fda6b54e0ffa1b2d7ddff20
Joshua Sommers <js3289@nau.edu>
2016-05-17 11:52:28 -0700

    Keypress
    Separated keypress into another .js file to handle later. May move back
    later, but need to improve on it first

---
+++ keypress.js
@@ -0,0 +1,44 @@
+/*
+	Handles all keypresses in the game.
+	WASD - Movement
+	Up, down, left, right - Movement
+	I - inventory (to be added)
+	T - teleport home (to be added, much later)
+*/
+
+function onKeyDown(key) {
+    // W Key is 87 Up arrow is 38
+    if (key.keyCode === 87 || key.keyCode === 38) {
+		character.direction = 1;
+		if(!hasCollided()) {
+			character.sprite.y -= character.movement;
+		}
+		
+    }
+
+    // S Key is 83 Down arrow is 40
+    else if (key.keyCode === 83 || key.keyCode === 40) {
+		character.direction = 0;
+		if(!hasCollided()) {
+			character.sprite.y += character.movement;
+		}
+    }
+
+    // A Key is 65 Left arrow is 37
+    if (key.keyCode === 65 || key.keyCode === 37) {
+		character.direction = 2;
+		character.sprite.scale.x = -1;
+		if(!hasCollided()) {
+			character.sprite.x -= character.movement;
+		}
+    }
+
+    // D Key is 68 Right arrow is 39
+    else if (key.keyCode === 68 || key.keyCode === 39) {
+		character.direction = 3;
+		character.sprite.scale.x = 1;
+		if(!hasCollided()) {
+			character.sprite.x += character.movement;
+		}
+    }
+}
