fe62e12f042b11a96f8ccd31df5b1033e0ef9698
Joshua Sommers <js3289@nau.edu>
2016-05-17 21:42:41 -0700

    Collision update
    Better collision detection, added collision detection between randomly
    spawned objects so that no 2 objects can collide while they spawn. This
    fixed a bug where the game was unbeatable because a coin spawned inside
    a stump.

--- collision.js
+++ collision.js
@@ -1,4 +1,4 @@
-function hasCollided() {
+function charHasCollided() {
 	var charY = character.sprite.y;
 	var charX = character.sprite.x;
 	
@@ -8,11 +8,11 @@ function hasCollided() {
 	var charEndX = charX + character.sprite.width / 2;
 	var charEndY = charX + character.sprite.width / 2;
 	
-	for(var i = 0; i < stage.children.length -1; i++) {
+	for(var i = 0; i < stage.children.length; i++) {
 		collision = false;
 		var temp = stage.getChildAt(i);
 		
-		if (temp.collides === true) {
+		if (temp.collides === true && temp.name != "player") {
 			var childY = temp.y;
 			var childX = temp.x;
 
@@ -22,20 +22,35 @@ function hasCollided() {
 			var childEndX = childX + temp.width / 2;
 			var childEndY = childY + temp.height / 2;
 
-			if(charX >= childBaseX && charX <= childEndX && charY >= childBaseY && charY <= childEndY) {
-				stage.removeChild(temp);
-				character.coins += 100;
-				coins.splice(0, 1);
-				collision = true;
+			//if(charX >= childBaseX && charX <= childEndX && charY >= childBaseY && charY <= childEndY) {
+			if(character.sprite.x < temp.x + temp.width / 2 && character.sprite.x + character.sprite.width / 2> temp.x && character.sprite.y < temp.y + temp.height / 2 && character.sprite.height / 2 + character.sprite.y > temp.y){
+				if(temp.name === "coin"){
+					stage.removeChild(temp);
+					character.coins += 100;
+					coins.splice(0, 1);
+					collision = true;
+				}
+				
+				else if(temp.name === "stump"){
+					collision = true;
+					sprite = new EnhSprite("losing", false, TextureImage("Assets/png/you-lose.png") );
+					losingC.visible = true;
+					losingC.addChild(sprite);
+					sprite.anchor.x = MIDDLE;
+					sprite.anchor.y = MIDDLE;
+					sprite.position.x = WIDTH / 2;
+					sprite.position.y = HEIGHT / 2;
+					stage.addChild(losingC);
+				}
 				
 				if(coins.length == 0) {
-					this.sprite = new EnhSprite("winning", false, TextureImage("Assets/png/you-win.png") );
+					sprite = new EnhSprite("winning", false, TextureImage("Assets/png/you-win.png") );
 					winningC.visible = true;
-					winningC.addChild(this.sprite);
-					this.sprite.anchor.x = MIDDLE;
-					this.sprite.anchor.y = MIDDLE;
-					this.sprite.position.x = WIDTH / 2;
-					this.sprite.position.y = HEIGHT / 2;
+					winningC.addChild(sprite);
+					sprite.anchor.x = MIDDLE;
+					sprite.anchor.y = MIDDLE;
+					sprite.position.x = WIDTH / 2;
+					sprite.position.y = HEIGHT / 2;
 					stage.addChild(winningC);
 				}
 
@@ -45,4 +60,62 @@ function hasCollided() {
 	
 	return collision;
 	
+}
+
+function objSpawnCollided(ent1, objects) {
+	collision = false;
+	
+	for(var i = 0; i < objects.length; i++) {
+		ent2 = objects[i];
+		//alert("height: " + ent1.sprite.height + " width: " + ent1.sprite.width + " height2: " + ent2.sprite.height + " width2: " + ent2.sprite.width);
+	
+		if(ent2.sprite.collides && ent1.sprite.collides) {
+			var ent1X = ent1.sprite.x;
+			var ent1Y = ent1.sprite.y;
+		
+			var ent1BaseX = ent1X - ent1.sprite.width / 2;
+			var ent1BaseY = ent1Y - ent1.sprite.height / 2;
+		
+			var ent1EndX = ent1X + ent1.sprite.width / 2;
+			var ent1EndY = ent1Y + ent1.sprite.height / 2;
+		
+		
+			var ent2X = ent2.sprite.x;
+			var ent2Y = ent2.sprite.y;
+			
+			var ent2BaseX = ent2X - ent2.sprite.width / 2;
+			var ent2BaseY = ent2Y - ent2.sprite.height / 2;
+		
+			var ent2EndX = ent2X + ent2.sprite.width / 2;
+			var ent2EndY = ent2Y + ent2.sprite.height / 2;
+			
+			//if(ent2BaseX <= ent1EndX && ent2BaseY <= ent1EndY) {
+			//	collision = true;
+			//}
+			//if((ent2BaseX >= ent1BaseX || ent2EndX >= ent1BaseX) && (ent2BaseX <= ent1EndX || ent2EndX <= ent1EndX) && (ent2BaseY >= ent1BaseY || ent2EndY >= ent1BaseY) && (ent2BaseY <= ent1EndY || ent2EndY <= ent1EndY)){
+			//if(((ent2BaseX >= ent1BaseX && ent2BaseX <= ent1EndX) || (ent2EndX >= ent1BaseX && ent2EndX <= ent1EndX)) && ((ent2BaseY >= ent1BaseY && ent2BaseY <= ent1EndY) || ent2EndY >= ent1BaseY && ent2EndY <= ent1EndY)){	
+			//	collision = true;
+			//	alert("true");
+			//}
+			if(ent1.sprite.x < ent2.sprite.x + ent2.sprite.width && ent1.sprite.x + ent1.sprite.width > ent2.sprite.x && ent1.sprite.y < ent2.sprite.y + ent2.sprite.height && ent1.sprite.height + ent1.sprite.y > ent2.sprite.y) {
+				collision = true;
+			}
+			
+			//if((ent2BaseX >= ent1BaseX || ent2EndX >= ent1BaseX) && (ent2BaseX <= ent1EndX || ent2EndX <= ent1EndX)) {
+			//	collision = true;
+			//}
+		//  if(charX >= childBaseX && charX <= childEndX && charY >= childBaseY && charY <= childEndY) {
+			//if((ent2BaseX || ent2EndX) >= ent1BaseX && (ent2BaseX || ent2EndX) <= ent1EndX && (ent2BaseY || ent2EndY) >= ent1BaseY && (ent2BaseY || ent2EndY) <= ent1EndY){
+			//	alert("hi");
+			//	collision = true;
+			//}
+			//if(ent2BaseX || ent2EndX >= ent1BaseX && ent2BaseX || ent2EndX <= ent1EndX && ent2BaseY || ent2EndY >= ent1BaseY && ent2BaseY || ent2EndY <= ent1EndY
+			//if(ent2X >= ent1BaseX && ent2X <= ent1EndX && ent2Y >= ent1BaseY && ent2Y <= ent1EndY) {
+			//	alert("true");
+			//	collision = true;
+			//}
+		}
+	}
+	
+	return collision
 }
